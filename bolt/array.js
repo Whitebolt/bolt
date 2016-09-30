96013ad9ea66811032849ad9d9022401492c30db
Stephen <stephen@whitebolt.net>
2016-09-30 10:38:49 +0100

    Add full tests for array.js and pad-out methods to make more robust
    Obviously, testing adds new robustness concepts so function have been padded-out to ensure they throw correctly and user con finely control their action.

--- bolt/array.js
+++ bolt/array.js
@@ -5,38 +5,93 @@
  */
 
 /**
- * Always return an array.  If the provided parameter is an array then return
- * it as-is.  If provided param is not an array return param as first item
- * of an array. If a convertFunction is supplied the default non-array to array
+ * Convert a non-array to an array using the formula: if =undefined then make empty array, otherwise create new array
+ * making the first item the supplied value.
+ *
+ * @private
+ * @param {*} ary     Value to convert.
+ * @returns {Array}   Converted value.
+ */
+function _makeArrayConvertFunction(ary) {
+  return ((ary===undefined)?[]:[ary]);
+}
+
+/**
+ * Always return an array.  If the provided parameter is an array then return it as-is.  If provided param is not an
+ * array return param as first item of an array. If a convertFunction is supplied the default non-array to array
  * conversion can be overridden.
  *
- * Function is useful when you always need a value to be array to use array
- * functions (such as map or forEach) on it but cannot guarantee it will be.
+ * Function is useful when you always need a value to be array to use array functions (such as map or forEach) on it
+ * but cannot guarantee it will be.
  *
+ * Will convert 'undefined' to an empty array.
+ *
+ * @static
  * @public
- * @param {*} ary                                     Item to return or
- *                                                    convert to an array.
- * @param {function} [convertFunction=(ary)=>[ary]]   Function used to convert
- *                                                    to an array if not
- *                                                    already one.
- * @returns {Array}                                   New array or supplied
- *                                                    parameter returned.
+ * @param {*} ary                                                   Item to return or convert to an array.
+ * @param {function} [convertFunction=_makeArrayConvertFunction]    Function used to convert to an array if not
+ *                                                                  already one.
+ * @returns {Array}                                                 New array or supplied parameter returned.
  */
-function makeArray(ary, convertFunction=(ary)=>[ary]) {
+function makeArray(ary, convertFunction=_makeArrayConvertFunction) {
   return (Array.isArray(ary) ? ary : convertFunction(ary));
 }
 
 /**
  * Sort an array according to it item's priority property.
  *
- * @param {Object} a    First sort item.
- * @param {Object} b    Second sort item.
- * @returns {integer}   Sort order for items a & b (-1, 0, or 1).
+ * @static
+ *
+ * @throws {SyntaxError}                When no arguments supplied.
+ * @throws {RangeError}                 When direction is set to something other than ASC or DESC.
+ * @throws {TypeError}                  When type of first parameter is either an object or string when one parameter
+ *                                      supplied.
+ *
+ * @param {Object|string} a             First sort item. If this is the only parameter then assume a new sorter function
+ *                                      is and this is the property to sort on.
+ * @param {string} [a.priority]         If only one argument supplied and this property is present return sorter with
+ *                                      this as the property to sort on.
+ * @param {string} [b.direction='ASC']  If only one argument supplied and this property is present return a sorter, which
+ *                                      sorts according to direction here (either: ASC or DESC).
+ * @param {Object} [b]                  Second sort item.
+ * @returns {integer|function}          Sort order for items a & b, returns either -1, 0 or 1. If one parameter was
+ *                                      supplied then return a sort function instead.
  */
 function prioritySorter(a, b) {
-  let aP = (a !== undefined ? (a.hasOwnProperty('priority') ? a.priority : 1) : 1);
-  let bP = (a !== undefined ? (b.hasOwnProperty('priority') ? b.priority : 1) : 1);
-  return ((aP > bP)?1:((aP < bP)?-1:0));
+  let sortProperty = 'priority';
+  let direction = 'ASC';
+
+  let sorter = (a, b)=>{
+    let aP = (a !== undefined ? (a.hasOwnProperty(sortProperty) ? a[sortProperty] : 0) : 0);
+    let bP = (a !== undefined ? (b.hasOwnProperty(sortProperty) ? b[sortProperty] : 0) : 0);
+
+    if (direction === 'ASC') {
+      return ((aP > bP)?1:((aP < bP)?-1:0));
+    } else if (direction === 'DESC') {
+      return ((aP > bP)?-1:((aP < bP)?1:0));
+    } else {
+      throw new RangeError('Sort direction for prioritySorter() should be either ASC or DESC');
+    }
+  };
+
+  if (arguments.length > 1) return sorter(a, b);
+  if (arguments.length === 0) {
+    throw new SyntaxError('No arguments supplied to prioritySorter()');
+  }
+
+  if (bolt.isString(a)) {
+    sortProperty = a;
+  } else if (bolt.isObject(a)) {
+    if (a.hasOwnProperty('sortProperty')) sortProperty = a.sortProperty.toString();
+    if (a.hasOwnProperty('direction')) direction = a.direction.toString().toUpperCase().trim();
+    if ((direction !== 'ASC') && (direction !== 'DESC')) {
+      throw new RangeError('Sort direction for prioritySorter() should be either ASC or DESC');
+    }
+  } else {
+    throw new TypeError('Wrong argument type supplied as first parameter for prioritySorter()');
+  }
+
+  return sorter;
 }
 
 module.exports = {
--- test/bolt/array.js
+++ test/bolt/array.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
-const bolt = Object.assign(
+
+global.bolt = Object.assign(
   require('lodash'),
   require(getFilePathForSubject())
 );
@@ -15,10 +16,12 @@ describe('bolt.array', ()=>{
       assert.isArray(bolt.makeArray(1));
       assert.isArray(bolt.makeArray("1"));
       assert.isArray(bolt.makeArray({}));
+      assert.isArray(bolt.makeArray(null));
       assert.deepEqual(bolt.makeArray(1), [1]);
       assert.deepEqual(bolt.makeArray("1"), ["1"]);
       assert.deepEqual(bolt.makeArray({}), [{}]);
       assert.deepEqual(bolt.makeArray({1:"test"}), [{1:"test"}]);
+      assert.deepEqual(bolt.makeArray(null), [null]);
     });
 
     it('Should not convert if already an array', ()=>{
@@ -30,6 +33,111 @@ describe('bolt.array', ()=>{
       testArray1[0] = 0;
 
       assert.deepEqual(testArray1, testArray2);
+    });
+
+    it('Should not convert undefined to an empty array', ()=>{
+      assert.isArray(bolt.makeArray(undefined));
+      assert.equal(bolt.makeArray(undefined).length, 0);
+    });
+
+    it('Should use convert function if supplied is not an array', ()=>{
+      const convertFunc = (ary)=>'TEST';
+
+      assert.deepEqual(bolt.makeArray([1], convertFunc), [1]);
+      assert.deepEqual(bolt.makeArray([1,2,3,4], convertFunc), [1,2,3,4]);
+      assert.equal(bolt.makeArray(1, convertFunc), 'TEST');
+    });
+  });
+
+  describe('bolt.prioritySorter()', ()=>{
+    it('Should return 1 when priority of first value greater than second.', ()=>{
+      assert.equal(bolt.prioritySorter({priority:2},{priority:1}), 1);
+    });
+    it('Should return -1 when priority of first value less than second.', ()=>{
+      assert.equal(bolt.prioritySorter({priority:1},{priority:2}), -1);
+    });
+    it('Should return 0 when priority of first value equal to the second.', ()=>{
+      assert.equal(bolt.prioritySorter({priority:2},{priority:2}), 0);
+    });
+
+    it('Should sort an array according to priority property when used as sort function.', ()=>{
+      const unsorted = [
+        {priority:5}, {priority:1}, {priority:2}, {priority:10}, {priority:-1}
+      ];
+      const sorted = [
+        {priority:-1}, {priority:1}, {priority:2}, {priority:5}, {priority:10}
+      ];
+
+      assert.deepEqual(unsorted.sort(bolt.prioritySorter), sorted);
+    });
+
+    it('Should assume a priority of 0 when non present.', ()=>{
+      const unsorted = [
+        {priority:5}, {priority:1}, {_priority:2}, {priority:10}, {}
+      ];
+      const sorted = [
+        {_priority:2}, {}, {priority:1}, {priority:5}, {priority:10}
+      ];
+      assert.deepEqual(unsorted.sort(bolt.prioritySorter), sorted);
+    });
+
+    it('Supplying one argument as a string should build a new sorter with supplied argument as the sort property.', ()=>{
+      const sorter = bolt.prioritySorter('_priority');
+      const unsorted = [
+        {_priority:5}, {_priority:1}, {_priority:2}, {_priority:10}, {_priority:-1}
+      ];
+      const sorted = [
+        {_priority:-1}, {_priority:1}, {_priority:2}, {_priority:5}, {_priority:10}
+      ];
+
+      assert.isFunction(sorter);
+      assert.deepEqual(unsorted.sort(sorter), sorted);
+    });
+
+    it('Supplying one argument as a an object should build a new sorter taking the sortProperty as the property name to sort on.', ()=>{
+      const sorter = bolt.prioritySorter({sortProperty:'_priority'});
+      const unsorted = [
+        {_priority:5}, {_priority:1}, {_priority:2}, {_priority:10}, {_priority:-1}
+      ];
+      const sorted = [
+        {_priority:-1}, {_priority:1}, {_priority:2}, {_priority:5}, {_priority:10}
+      ];
+
+      assert.isFunction(sorter);
+      assert.deepEqual(unsorted.sort(sorter), sorted);
+    });
+
+    it('Supplying one argument as a an object should build a new sorter taking the sortProperty as the property name to sort on and direction as the search direction.', ()=>{
+      const sorterAsc = bolt.prioritySorter({sortProperty:'_priority', direction:'ASC'});
+      const sorterDesc = bolt.prioritySorter({sortProperty:'_priority', direction:'DESC'});
+      const unsorted = [
+        {_priority:5}, {_priority:1}, {_priority:2}, {_priority:10}, {_priority:-1}
+      ];
+      const sortedAsc = [
+        {_priority:-1}, {_priority:1}, {_priority:2}, {_priority:5}, {_priority:10}
+      ];
+      const sortedDesc = bolt.clone(sortedAsc).reverse();
+
+      assert.isFunction(sorterAsc);
+      assert.isFunction(sorterDesc);
+      assert.deepEqual(unsorted.sort(sorterAsc), sortedAsc);
+      assert.deepEqual(unsorted.sort(sorterDesc), sortedDesc);
+    });
+
+    it('Throws when no arguments supplied', ()=>{
+      assert.throws(()=>bolt.prioritySorter(), SyntaxError);
+      assert.throws(()=>bolt.prioritySorter(), 'No arguments supplied to prioritySorter()');
+    });
+
+    it('Throws when one argument supplied and it is neither an object or a string.', ()=>{
+      assert.throws(()=>bolt.prioritySorter(1), TypeError);
+      assert.throws(()=>bolt.prioritySorter(1), 'Wrong argument type supplied as first parameter for prioritySorter()');
+      assert.throws(()=>bolt.prioritySorter(null), 'Wrong argument type supplied as first parameter for prioritySorter()');
+    });
+
+    it('Throws when one argument supplied and direction is neither ASC or DESC', ()=>{
+      assert.throws(()=>bolt.prioritySorter({direction:'EQUAL'}), RangeError);
+      assert.throws(()=>bolt.prioritySorter({direction:'EQUAL'}), 'Sort direction for prioritySorter() should be either ASC or DESC');
     });
   });
 });
