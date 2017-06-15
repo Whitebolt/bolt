f2cc6ce85e4940fdf0578315aaaa0a34b935012c
Stephen <stephen@whitebolt.net>
2017-06-15 10:02:46 +0100

    Add inline documentation to shortcode.js
    Added inline documentation to shortcode methods.

--- bolt/shortcode.js
+++ bolt/shortcode.js
@@ -3,6 +3,14 @@
 const Promise = require('bluebird');
 const parser = require('shortcode-insert')();
 
+/**
+ * Parse shortcodes in a given document.
+ *
+ * @param {Object} component    Component object that the shortcode relates to.
+ * @param doc {Object}          Document to parse for shortcodes.
+ * @param properties            Document properties to parse shortcodes in.
+ * @returns {Promise}           Promise resolving when all shortcode parsing done.
+ */
 function parseShortcodes(component, doc, properties=[]) {
   return Promise.all(properties.map(property=>{
     if (doc.hasOwnProperty(property)) {
@@ -13,6 +21,14 @@ function parseShortcodes(component, doc, properties=[]) {
   }));
 }
 
+/**
+ * Load shortcode parsing functions into app.
+ *
+ * @param {Object} component    Component object to import shortcodes into.
+ * @param {string|Array} roots  Root folder(s) to search for shortcode functions in.
+ * @param {Object} importObject Object to import into.
+ * @returns {Promise}           Promise resolving to the supplied component.
+ */
 function loadShortcodes(component, roots=bolt.getApp(component).config.root, importObj=bolt.getApp(component).shortcodes) {
   return bolt
     .importIntoObject({roots, importObj, dirName:'shortcodes', eventName:'loadedShortcode'})
