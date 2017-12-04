86c00054b7a5bd1661625dfd7d876f38ca422168
Stephen <stephen@whitebolt.net>
2017-12-04 13:00:31 +0000

    Extract ready() from server.js
    Move ready() into separate module.  This can be replaced by a bolt event at some point.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ Error.stackTraceLimit = Infinity;
 let [configDone, boltLoaded] = [false, false];
 
 const bolt = {requireX: require('require-extra')};
-const ready = _getReady();
+const ready = require('./lib/ready')(bolt, ()=>boltLoaded);
 require('./lib/requirex')(bolt, ()=>boltLoaded);
 require('./lib/platformScope')(bolt, __dirname);
 const packageConfig = bolt.requireX.sync('./package.json').config || {};
@@ -14,32 +14,6 @@ const packageConfig = bolt.requireX.sync('./package.json').config || {};
 
 global.startTime = process.hrtime();
 
-/**
- * Setup ready callback.
- *
- * @private
- * @returns {Function}      The ready function to fire all on ready events.
- */
-function _getReady() {
-  const readyCallbacks = new Set();
-
-  bolt.ready = (hook, handler=hook)=>{
-    const _handler = ((hook !== handler) ? ()=>bolt.emit(hook, handler) : handler);
-
-    if (!boltLoaded) {
-      readyCallbacks.add(_handler);
-      return ()=>readyCallbacks.delete(_handler);
-    } else {
-      _handler();
-      return ()=>{};
-    }
-  };
-
-  return ()=>{
-    readyCallbacks.forEach(handler=>handler());
-    readyCallbacks.clear();
-  }
-}
 
 /**
  * Start the app loading process.
