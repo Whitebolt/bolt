d7868ed423b9eac9bee7a4410bcd8e50f2cff312
Stephen <stephen@whitebolt.net>
2017-12-04 12:49:47 +0000

    Refactor some code out of server.js
    Extracted some of the extra code from server.js so as not to clutter it.  Bit awkward and needs further refactoring but is better than it was.

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ Error.stackTraceLimit = Infinity;
 
 let [configDone, boltLoaded] = [false, false];
 
-const eventsStack = [];
-const requireX = _getRequireX();
-const bolt = _createPlatformScope();
-const packageConfig = requireX.sync('./package.json').config || {};
-const ready = _getReady(bolt);
-const path = require('path');
+const bolt = {requireX: require('require-extra')};
+const ready = _getReady();
+require('./lib/requirex')(bolt, ()=>boltLoaded);
+require('./lib/platformScope')(bolt, __dirname);
+const packageConfig = bolt.requireX.sync('./package.json').config || {};
 
 
 global.startTime = process.hrtime();
@@ -42,116 +41,6 @@ function _getReady() {
   }
 }
 
-bolt.ready(()=>{
-  bolt.ModuleSetScopeEvent = class ModuleSetScopeEvent extends bolt.Event {};
-  bolt.ModuleEvaluateEvent = class ModuleEvaluateEvent extends bolt.Event {};
-});
-
-/**
- * Get and setup requireX
- *
- * @private
- * @returns {requireX}    The requireX module.
- */
-function _getRequireX() {
-  const requireX = require('require-extra');
-
-  function moduleLoaded(event) {
-    bolt.emit('loadedModule', event.target, (((event.duration[0] * 1000000000) + event.duration[1]) / 1000000));
-  }
-
-  function getEmitFunction(event) {
-    return !!event.sync?bolt.emitSync:bolt.emit;
-  }
-
-  function evaluate(eventName, event) {
-    return getEmitFunction(event)(eventName, new bolt.ModuleEvaluateEvent({
-      type:eventName,
-      target:event.target,
-      source:event.source,
-      config: event.moduleConfig,
-      parserOptions: event.parserOptions,
-      data: event.data,
-      scope: event.moduleConfig.scope,
-      sync: event.sync
-    }));
-  }
-
-  function setScope(eventName, event) {
-    return getEmitFunction(event)(eventName, new bolt.ModuleSetScopeEvent({
-      type:eventName,
-      target:event.target,
-      source:event.source,
-      scope: event.moduleConfig.scope,
-      sync: event.sync
-    }));
-  }
-
-  requireX.on('evaluate', event=>{
-    if (boltLoaded) {
-      const ext = path.extname(event.target);
-      if (ext.charAt(0) === '.') {
-        event.moduleConfig.scope = event.moduleConfig.scope || {};
-        return bolt.runSeries(!event.sync, [
-          ()=>setScope(bolt.camelCase(`moduleSetScope_${ext.substring(1)}`), event),
-          ()=>evaluate(bolt.camelCase(`moduleEvaluate_${ext.substring(1)}`), event)
-        ]);
-      }
-    }
-  });
-
-  requireX.on('evaluated', event=>{
-    if (boltLoaded) {
-      while (eventsStack.length) moduleLoaded(eventsStack.pop());
-      moduleLoaded(event);
-    } else {
-      eventsStack.push(event);
-    }
-  }).on('error', error=>{
-    console.log(error);
-  });
-
-  return requireX;
-}
-
-/**
- * Create the bolt object.
- *
- * @private
- * @returns {Object}
- */
-function _createBoltObject() {
-  return Object.assign(
-    {},
-    requireX.sync('lodash'),
-    requireX.sync('map-watch'), {
-      annotation: new (requireX.sync('object-annotations'))()
-    }
-  );
-}
-
-/**
- * Create and setup the scopes used in the platform via require-extra.  Return bolt object.
- *
- * @private
- * @returns {Object}    The bolt object.
- */
-function _createPlatformScope() {
-  const scope = {
-    bolt: _createBoltObject(),
-    boltRootDir: __dirname,
-    express: requireX.sync('express')
-  };
-
-  requireX.on('evaluate', event=>{
-    if (event.moduleConfig.filename.indexOf('/node_modules/') === -1) event.moduleConfig.scope = scope;
-  });
-
-  scope.boltAppID = requireX.sync('./bolt/string').randomString();
-
-  return scope.bolt;
-}
-
 /**
  * Start the app loading process.
  *
@@ -176,7 +65,7 @@ async function appLauncher(config) {
   if (!configDone) {
     configDone = true;
     if (!boltLoaded) {
-      await requireX.import('./bolt/', {
+      await bolt.requireX.import('./bolt/', {
         merge: true,
         imports: bolt,
         excludes: packageConfig.appLaunchExcludes,
@@ -207,10 +96,10 @@ async function pm2Controller() {
     parent: __filename
   };
   if (process.getuid && process.getuid() === 0) boltImportOptions.includes = packageConfig.pm2LaunchIncludes;
-  await requireX.import('./bolt/', boltImportOptions);
+  await bolt.requireX.import('./bolt/', boltImportOptions);
   boltLoaded = true;
   ready();
-  const args = await requireX('./cli');
+  const args = await bolt.requireX('./cli');
 
   return Promise.all(args._.map(cmd=>{
     if (args.cmd.hasOwnProperty(cmd)) return args.cmd[cmd](args);
