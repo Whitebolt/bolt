c8ffabc3a7ac783649cd815381e9ab4e61a30aca
Stephen <stephen@whitebolt.net>
2017-12-05 10:13:05 +0000

    Redesign display injector
    Redesigned the display injector slightly to allow non-stringification when viaView is set.  Also, now passing parent to view.

--- bolt/controller/injectors.js
+++ bolt/controller/injectors.js
@@ -57,11 +57,19 @@ function isJson(component, extraParams, method) {
 }
 
 function display(component, extraParams, method) {
-  const {isJson, viaView, res, doc, req, view} = injectorReflect(['isJson', 'viaView', 'res', 'doc', 'req', 'view'], component, extraParams, method);
+  const {isJson, viaView, res, doc, req, view, values, parent} = injectorReflect(
+    ['isJson', 'viaView', 'res', 'doc', 'req', 'view', 'values', 'parent']
+    , component, extraParams, method
+  );
+  const stringify = (('stringify' in values) ? bolt.toBool(values.stringify, bolt.getDefault('bool.true').concat(undefined, 'undefined')) : true);
 
   return async (docField, viewName)=>{
-    if (isJson) return viaView?JSON.stringify(docField?doc[docField]:doc):res.json(docField?doc[docField]:doc);
-    const html = await view(viewName, doc, req);
+    if (isJson) {
+      const value = docField?doc[docField]:doc;
+      if (viaView) return (stringify?JSON.stringify(value):value);
+      return res.json(value);
+    }
+    const html = await view(viewName, doc, req, parent);
     return viaView?html:res.send(html);
   };
 }
