45c30b568450d07497d29f20f2fd6c7f6bfe1cbf
Stephen <stephen@whitebolt.net>
2018-01-12 09:44:46 +0000

    Bug fix redux load
    Was only loading one of each type!

--- bolt/redux.js
+++ bolt/redux.js
@@ -1,21 +1,28 @@
 'use strict';
 
+const boltKey = '__redux';
+
 function importer({root, type, onload, extensions='.jsx', basedir=__dirname, parent=__filename}) {
+
 	const importPaths = bolt.makeArray(root).map(root=>`${root}/${type}/`);
 	return require.import(importPaths, {
 		extensions,
 		basedir,
 		parent,
+		retry: true,
 		onload: (filename, exports)=>{
 			Object.keys(exports).forEach(exportName=>{
 				if (exportName === 'default') return undefined;
 				bolt.ReduxBolt[type][exportName] = exports[exportName];
 			});
 
-			bolt.__redux = bolt.__redux || {};
-			if (!(type in bolt)) bolt.__redux[type] = new Set();
-			bolt.__redux[type].add(filename);
+			bolt[boltKey] = bolt[boltKey] || {};
+			if (!(type in bolt[boltKey])) bolt[boltKey][type] = new Set();
+			bolt[boltKey][type].add(filename);
 			if (onload) onload(filename, exports);
+		},
+		onerror: error=>{
+			throw error;
 		}
 	});
 }
