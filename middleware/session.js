b6a5668fd6848e43c9e00b76961072ccf4ab39ca
Stephen <stephen@whitebolt.net>
2018-03-06 15:47:47 +0000

    Fix the session length tgo actually use config value
    Typo was stopping the config value being used.

--- middleware/session.js
+++ middleware/session.js
@@ -9,34 +9,34 @@ const session = require('express-session');
  * @param {BoltApplication} app   The bolt application instance.
  */
 function init(app) {
-  // @annotation priority 1
+	// @annotation priority 1
 
-  app.set('trust proxy', 1);
+	app.set('trust proxy', 1);
 
-  const store = bolt['load'+bolt.capitalize(app.config.sessionStore || 'mongo')].sessionStore(session, app);
+	const store = bolt['load'+bolt.capitalize(app.config.sessionStore || 'mongo')].sessionStore(session, app);
 
-  let sessionMiddleware = session({
-    name: 'sessionId',
-    secret: app.config.secret,
-    cookie: {
-      secure: true,
-      maxAge: app.sessionLength || 60 * 60 * 1000,
-      httpOnly: true
-    },
-    store,
-    resave: true,
-    saveUninitialized: true
-  });
+	let sessionMiddleware = session({
+		name: 'sessionId',
+		secret: app.config.secret,
+		cookie: {
+			secure: true,
+			maxAge: app.config.sessionLength || 60 * 60 * 1000,
+			httpOnly: true
+		},
+		store,
+		resave: true,
+		saveUninitialized: true
+	});
 
-  app.use(sessionMiddleware, (req, res, next)=>{
-    if (req.session) {
-      if (req.websocket) req.session.socketId = req.websocket.id;
-      if (req.session.socketId && !app.io.sockets.connected.hasOwnProperty(req.session.socketId)) delete req.session.socketId;
-      req.session.save();
-    }
+	app.use(sessionMiddleware, (req, res, next)=>{
+		if (req.session) {
+			if (req.websocket) req.session.socketId = req.websocket.id;
+			if (req.session.socketId && !app.io.sockets.connected.hasOwnProperty(req.session.socketId)) delete req.session.socketId;
+			req.session.save();
+		}
 
-    next();
-  });
+		next();
+	});
 }
 
 module.exports = init;
