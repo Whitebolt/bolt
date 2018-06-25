2bdcea82e1d2ce70ab73994143ec9ea8c46538f7
Stephen <stephen@whitebolt.net>
2018-06-25 11:44:08 +0100

    Improve done() injector
    Improved the done() injector further by allowing json to be sent back.

--- bolt/controller/injectors.js
+++ bolt/controller/injectors.js
@@ -93,8 +93,16 @@ const injectors = Object.freeze({
 	done: component=>{
 		return (status=200, message)=> {
 			component.done = !!status;
-			if (!bolt.isNaN(status)) component.status = status;
-			if (!!message) component.statusMessage = message;
+			if (!bolt.isNaN(status)) component.res.statusCode = status;
+			if (!!message) {
+				if (bolt.isString(message)) {
+					component.res.statusMessage = message;
+				} else if (bolt.isObject(message)) {
+					component.req.doc = component.req.doc || {};
+					Object.assign(component.req.doc, message);
+					component.sendFields = [...(component.sendFields || []), ...Object.keys(component.req.doc)];
+				}
+			}
 		}
 	},
 	app: component=>component.req.app,
