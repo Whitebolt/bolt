652ea7aef84dbd19bc82b52896f50fa282818e1d
Stephen <stephen@whitebolt.net>
2018-07-23 07:55:31 +0100

    Stop serving non-existent directories
    We were serving directories that did not exist, fixed that :-)

--- middleware/static.js
+++ middleware/static.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const serve = require('serve-static');
 const defaultOptions = {
 	index:false
@@ -14,27 +15,47 @@ const defaultOptions = {
 function init(app) {
 	// @annotation priority 10
 
-	/**
-	 * @todo check if /public exists first
-	 */
-	bolt.get(app, 'locals.root', []).forEach(rootDir=>{
-		app.use(serve(`${rootDir}/private/${app.locals.name||'unknown'}`, {...defaultOptions}));
-		app.use(serve(`${rootDir}/public/`, {...defaultOptions}));
-		app.use(serve(`${rootDir}/upload/`, {...defaultOptions}));
-	});
+	const publicDirs = [path.join('private', app.locals.name||'unknown'), 'public', 'upload'];
+	const exportedModules = new Set();
+
+	bolt.chain(bolt.get(app, 'locals.root', []))
+		.map(root=>publicDirs.map(publicDir=>{
+			const dirToServe = path.join(root, publicDir);
+			if (bolt.fileExistsSync(dirToServe)) return ['/', dirToServe];
+		}))
+		.flatten()
+		.filter(dir=>dir)
+		.forEach(([mountPath, dirPath])=>{
+			app.use(mountPath, serve(dirPath, {...defaultOptions}));
+			bolt.emit('mountFilesystem', dirPath, mountPath);
+		})
+		.value();
 
 	bolt.chain(bolt.get(app, `locals.nodeModulesServe`, {}))
 		.keys()
-		.forEach(rootDir=>{
-			const moduleServe = app.locals.nodeModulesServe[rootDir];
-			if (moduleServe.modules) bolt.makeArray(moduleServe.modules).forEach(moduleName=>{
-				app.use(
-					`${moduleServe.path || '/lib'}/${moduleName}`,
-					serve(`${rootDir}node_modules/${moduleName}`, {...defaultOptions})
-				);
+		.reverse()
+		.map(root=>{
+			const moduleServe = app.locals.nodeModulesServe[root];
+			if (moduleServe.modules) return bolt.makeArray(moduleServe.modules).map(_moduleName=>{
+				const [moduleName, serveAs=_moduleName] = _moduleName.split(':');
+				const servePath = path.join(moduleServe.path || 'lib', serveAs);
+				const lookup = `${servePath}:${moduleName}`;
+				if (!exportedModules.has(lookup)) {
+					const dirToServe = path.join(root, 'node_modules', moduleName);
+					if (bolt.fileExistsSync(dirToServe)) {
+						exportedModules.add(lookup);
+						return [servePath, dirToServe];
+					}
+				}
 			});
 		})
-		.value()
+		.flatten()
+		.filter(dir=>dir)
+		.forEach(([mountPath, dirPath])=>{
+			app.use(mountPath, serve(dirPath, {...defaultOptions}));
+			bolt.emit('mountFilesystem', dirPath, mountPath);
+		})
+		.value();
 }
 
 module.exports = init;
