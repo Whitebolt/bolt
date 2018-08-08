cd6bad7b63f6cd0f5bbd3cb99fb43630b0c86dfa
Stephen <stephen@whitebolt.net>
2018-08-08 09:33:37 +0100

    Add a gulp injection methhod of getBabelPlugins
    When using bable plugins are resolved from the calling script meaning it can't require locally.  Added a method for parse the plugin strings and requiring them locally (doing what babel doses).  Maybe I need to PR babel?

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,35 @@ const xRollup = /^rollup/;
 argv.appName = argv.name;
 
 
+function reformatBabelPluginId(id) {
+	const [scope, pluginName] = id.split('/');
+	const _pluginName = `plugin-${(!!pluginName?pluginName:scope)}`.replace('plugin-plugin-', 'plugin-');
+	return ((!!pluginName)?[scope,_pluginName].join('/'):_pluginName);
+}
+
+function getBabelPlugins(plugins, require) {
+	const log = new Set();
+	return bolt.chain(bolt.makeArray(plugins))
+		.map(id=>{
+			const isArray = Array.isArray(id);
+			if (!isArray && !bolt.isString(id)) return id
+			const _id = isArray?id[0]:id;
+			if (log.has(_id)) return;
+			log.add(_id);
+			const plugin = require(reformatBabelPluginId(_id));
+			if (!isArray) return plugin;
+			id[0] = plugin;
+			return id;
+		})
+		.filter(id=>!!id)
+		.value();
+}
+
+
 get('resolvers')
+	.add(({param, require})=>{
+		if (param === 'getBabelPlugins') return plugins=>getBabelPlugins(plugins, require);
+	})
 	.add(({param, require})=>{
 		if (xRollup.test(param)) return require(`rollup-plugin-${bolt.kebabCase(param.replace(xRollup, ''))}`);
 	})
